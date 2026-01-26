e60cb606c0d36cb25a6aa7525b1feb3064c03301
jpdhanyaa <jpdhanyaa@gmail.com>
2026-01-26 11:51:44 +0530

    Update frontend and backend

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,27 @@
-const express = require("express")
-const app=express()
-app.use(express.json())
-app.use(express.urlencoded({ extended:true}))
-app.use(express.static("public"))
-const complaintRoutes=require("./routes/complaintRoutes")
-app.use(complaintRoutes)
-app.get("/",(req,res)=>{
-    res.send("Server is running")
-})
-app.listen(3000)
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static(path.join(__dirname, "../frontend")));
+
+app.get("/api/test", (req, res) => {
+  res.send("Backend API is working");
+});
+
+app.post("/complaints", (req, res) => {
+  const { studentName, issueTitle, description } = req.body;
+
+  console.log("Complaint received:");
+  console.log(studentName, issueTitle, description);
+
+  res.send("Complaint received successfully");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
